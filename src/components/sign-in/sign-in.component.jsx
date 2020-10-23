25b2397dd6ca68079d48079b87513c6a44eb8ca5
Huat Zhi <huatzhi@gmail.com>
2020-10-23 04:13:49 +0000

    firebase setting

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -40,6 +42,7 @@ class SignIn extends React.Component{
           <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} required label="Password" />
 
           <CustomButton type='submit'>Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
         </form>
       </div>
     )
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyBGpmYbgxuyeHD0rwLD8No4VD2avpRhh7I",
+  authDomain: "gunpla-shop.firebaseapp.com",
+  databaseURL: "https://gunpla-shop.firebaseio.com",
+  projectId: "gunpla-shop",
+  storageBucket: "gunpla-shop.appspot.com",
+  messagingSenderId: "368534784616",
+  appId: "1:368534784616:web:9965c930b6f63879b1c9dd",
+  measurementId: "G-XCXB0XBQT5"
+};
+
+firebase.initializeApp(config);
+
+// may want to remove
+firebase.analytics();
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+export const signInWithGoogle = () => auth.signInWithPopup();
+
+export default firebase;
